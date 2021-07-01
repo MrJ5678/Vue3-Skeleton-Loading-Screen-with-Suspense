f12933c70ff24604cc085bac9a9e4c9dc6fa9e77
hhhhhq <Mr.j@tom.com>
2021-07-01 11:13:06 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <Suspense>
+    <template #default>
+      <ProfileCard />
+    </template>
+    <template #fallback>
+      <ProfileCardSkeleton />
+    </template>
+  </Suspense>
+</template>
+
+<script setup>
+import ProfileCard from "./components/ProfileCard.vue"
+import ProfileCardSkeleton from "./components/ProfileCardSkeleton.vue"
+
+// This starter template is using Vue 3 experimental <script setup> SFCs
+// Check out https://github.com/vuejs/rfcs/blob/script-setup-2/active-rfcs/0000-script-setup.md
+</script>
+
+<style>
+html,
+body {
+  background-color: #eee;
+}
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: #2c3e50;
+}
+</style>
---
+++ src/components/ProfileCardSkeleton.vue
@@ -0,0 +1,59 @@
+<!--
+ * @Author: hhhhhq
+ * @Date: 2021-07-01 10:42:56
+ * @LastEditors: hhhhhq
+ * @LastEditTime: 2021-07-01 10:55:51
+ * @Description: file content
+-->
+<template>
+  <div class="profile-card">
+    <div class="profile-image">
+      <img />
+    </div>
+    <div class="profile-info">
+      <span />
+      <h3 />
+      <p />
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.profile-image img {
+  width: 100%;
+  padding-top: 100%;
+  background-color: #eee;
+  animation: pulse-bg 1s infinite;
+}
+
+.profile-info span {
+  width: 100%;
+  height: 16px;
+  display: block;
+  animation: pulse-bg 1s infinite;
+}
+
+.profile-info h3 {
+  width: 250px;
+  height: 24px;
+  animation: pulse-bg 1s infinite;
+}
+
+.profile-info p {
+  width: 80%;
+  height: 16px;
+  animation: pulse-bg 1s infinite;
+}
+
+@keyframes pulse-bg {
+  0% {
+    background: #eee;
+  }
+  50% {
+    background: #eaeaea;
+  }
+  100% {
+    background: #eee;
+  }
+}
+</style>
